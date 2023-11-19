637d178a38473af73b35830bcd8aa2cd474ffd1d
Rikketz <jesussavi88@gmail.com>
2023-11-19 12:53:21 +0100

    add select language screen. Generate documents with the language change and auto-translated. Now it's possible to generate documents in PDF

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-
-
-
-import ButtonGeneral from './Components/buttonGeneral/buttonGeneral';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
-import Hamburguesa from './Components/Hamburguesa/Hamburguesa';
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import React, { useEffect, useState } from "react";
 import { API } from "./shared/services/api";
 import Register from "./pages/Register/Register";
+import DocumentTranslated from "./pages/DocumentTranslated/DocumentTranslated";
+import DocumentTranslated2 from "./pages/DocumentTranslated2/DocumentTranslated2";
+import GenerateInform from "./pages/GenerateInform/GenerateInform";
 export const Contexto = React.createContext();
 
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem("token"));
+  const [idioma, setIdioma] = useState("");
+  const languageSelectedList = useState([]);
 
   const checkSession = async () => {
     try {
@@ -30,21 +30,20 @@ function App() {
   }, []);
   return (
 
-    <Contexto.Provider value={{ token, setToken }}>
+    <Contexto.Provider value={{ token, setToken, idioma, setIdioma, languageSelectedList }}>
       <div className="App">
         <Router>
           <Routes>
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
-            {/* <Route
-              path="secure"
-              element={<AuthRoute component={<Secure />} />}
-            /> */}
+            <Route path="generateInform/inform1" element={<DocumentTranslated />} />
+            <Route path="generateInform/inform2" element={<DocumentTranslated2 />} />
+            <Route path="generateInform" element={<GenerateInform />} />
           </Routes>
         </Router>
 
       </div>
-    <Hamburguesa></Hamburguesa>
+
     </Contexto.Provider>
 
   );
