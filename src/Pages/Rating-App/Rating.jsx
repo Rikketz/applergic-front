37d527e869c8cd0d20cadaa955d99ea8b77c30cb
Julio-Gonzalo <juliogonzaloguadalupe@protonmail.com>
2023-11-21 10:34:46 +0000

    Arreglos en la página valoración

--- src/Pages/Rating-App/Rating.jsx
+++ src/Pages/Rating-App/Rating.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
@@ -12,6 +12,8 @@ import { Rating } from "primereact/rating";
 import { Link } from "react-router-dom";
 
 export default function RatingApp() {
+  const [value, setValue] = useState(null);
+
   return (
     <>
       <header className="header-rating">
@@ -30,7 +32,11 @@ export default function RatingApp() {
         <img src={applergic} alt="logo-applergic" className="img-main-rating" />
         <h3 className="h3-main-rating">¡Gracias por usar Applergic!</h3>
         <h4 className="h4-main-rating">Por favor, evalua tu experiencia.</h4>
-        <Rating cancel={false} />
+        <Rating
+          value={value}
+          onChange={(e) => setValue(e.value)}
+          cancel={false}
+        />
       </main>
       <footer className="footer-rating">
         <Link to="/main">
